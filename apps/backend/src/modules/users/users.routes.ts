337e0609186fd6660627e53c44ad6604e486a775
Michał C <64898993+mciezki@users.noreply.github.com>
2025-10-12 21:26:29 +0200

    feat(BE): add unique slug generator service & controller (#48)

--- apps/backend/src/modules/users/users.routes.ts
+++ apps/backend/src/modules/users/users.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { user } from "./controllers/user.controller";
 import { users } from "./controllers/users.controller";
+import { authMiddleware } from "../../middlewares/auth.middleware";
+import { slug } from "./controllers/slug.controller";
 
 const router = Router();
 
+router.get('/slug', authMiddleware, slug)
 router.get('/:slugOrId', user)
 router.get('/', users)
 
